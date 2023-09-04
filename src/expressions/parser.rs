14624e1285a0301cb6776ce370d4b49b42093119
Sven Karol <svenkarol+github@gmail.com>
2023-09-04 18:14:06 +0200

    Build syntax tree during parse

--- src/expressions/parser.rs
+++ src/expressions/parser.rs
@@ -1,103 +1,178 @@
-use crate::expressions::lexer::TokenType;
-use crate::expressions::lexer::ExpToken;
 use crate::expressions::lexer::tokenize;
+use crate::expressions::lexer::ExpToken;
+use crate::expressions::lexer::TokenType;
 use std::iter::Peekable;
 
 /// A parser of simple arithmetic expressions using an [Iterator] source.
-pub struct ExpParser<'a, T : Iterator<Item = &'a ExpToken>> {
-    source: Peekable<T>
+pub struct ExpParser<T: Iterator<Item = ExpToken>> {
+    source: Peekable<T>,
 }
 
-impl<'a, T : Iterator<Item = &'a ExpToken>> ExpParser<'a, T> {
+pub struct Node {
+    children: Vec<Tree>,
+    kind: NodeType,
+}
 
+impl Node {
+    pub const fn new(kind: NodeType) -> Node {
+        Node {
+            children: Vec::new(),
+            kind: kind,
+        }
+    }
+
+    pub fn add_child(&mut self, new_child: Tree) {
+        self.children.push(new_child);
+    }
+}
+
+pub enum Tree {
+    Node(Node),
+    Leaf(ExpToken),
+}
+
+pub enum NodeType {
+    Start,
+    Exp,
+    Expp,
+    Term,
+    Termp,
+}
+
+impl<T: Iterator<Item = ExpToken>> ExpParser<T> {
     /// Creates a fresh instance from the given iterator.
     pub fn new(source: Peekable<T>) -> Self {
         Self { source }
     }
-    
+
     /// Performs the parsing. Currently this only checks whether the content is an expression or not.
     pub fn parse(&mut self) -> bool {
         self.start().is_some()
     }
 
     /// Function that corresponds to the grammar's start symbol
-    fn start(&mut self) -> Option<()> {      
-        self.match_token(TokenType::INIT)
-            .map(|_|())
-            .and(self.consume().map(|_|()))
-            .and(self.exp())
-            .and(self.match_token(TokenType::END)).map(|_|())
-            .and(self.consume().map(|_|()))
-    }
-
-    fn exp(&mut self) -> Option<()> { 
-        self.term()
-            .and_then(|_|self.expp())
-    }
-
-    fn term(&mut self) -> Option<()> {
-        self.match_token(TokenType::NUM).map(|_|())
-            .and(self.consume().map(|_|()))
-            .and(self.termp())
-    }
-
-    fn expp(&mut self) -> Option<()> {
-        if self.match_token(TokenType::ADDOP).is_some() {
-            self.consume().map(|_|())
-                .and(self.term())
-                .and(self.expp())
-        }
-        else {
-            Some(())
-        }
-    }
-
-    fn termp(&mut self) -> Option<()> {
-        if self.match_token(TokenType::MULOP).is_some() {
-            self.consume().map(|_|Some(()))
-                .and(
-                    if self.match_token(TokenType::NUM).is_some() {
-                        self.consume().map(|_|Some(()))
-                            .and(self.termp())
+    fn start(&mut self) -> Option<Tree> {
+        match self.match_consume(TokenType::INIT) {
+            Some(_) => {
+                let mut start = Node::new(NodeType::Start);
+                match self.exp() {
+                    Some(exp_child) => {
+                        start.add_child(exp_child);
+                        match self.match_consume(TokenType::END) {
+                            Some(_) => Some(Tree::Node(start)),
+                            None => None,
+                        }
                     }
-                    else {
-                        None
-                    }
-                )
-        }
-        else {
-            Some(())
+                    None => None,
+                }
+            }
+            None => None,
         }
     }
 
-    fn match_token(&mut self, ttype: TokenType) -> Option<&&ExpToken> {
+    fn exp(&mut self) -> Option<Tree> {
+        match self.term() {
+            Some(term) => match self.expp() {
+                Some(expp) => {
+                    let mut exp = Node::new(NodeType::Exp);
+                    exp.add_child(term);
+                    exp.add_child(expp);
+                    Some(Tree::Node(exp))
+                }
+                None => None,
+            },
+            None => None,
+        }
+    }
+
+    fn term(&mut self) -> Option<Tree> {
+        match self.match_consume(TokenType::NUM) {
+            Some(tk_num) => match self.termp() {
+                Some(termp) => {
+                    let mut term = Node::new(NodeType::Term);
+                    term.add_child(Tree::Leaf(tk_num));
+                    term.add_child(termp);
+                    Some(Tree::Node(term))
+                }
+                None => None,
+            },
+            None => None,
+        }
+    }
+
+    fn expp(&mut self) -> Option<Tree> {
+        match self.match_consume(TokenType::ADDOP) {
+            Some(tk_add) => match self.term() {
+                Some(term) => match self.expp() {
+                    Some(expp) => {
+                        let mut this_expp = Node::new(NodeType::Expp);
+                        this_expp.add_child(Tree::Leaf(tk_add));
+                        this_expp.add_child(term);
+                        this_expp.add_child(expp);
+                        Some(Tree::Node(this_expp))
+                    }
+                    None => None,
+                },
+                None => None,
+            },
+            None => Some(Tree::Node(Node::new(NodeType::Expp))), // Epsilon
+        }
+    }
+
+    fn termp(&mut self) -> Option<Tree> {
+        match self.match_consume(TokenType::MULOP) {
+            Some(tk_mul) => match self.match_consume(TokenType::NUM) {
+                Some(tk_num) => match self.termp() {
+                    Some(termp) => {
+                        let mut this_termp = Node::new(NodeType::Termp);
+                        this_termp.add_child(Tree::Leaf(tk_mul));
+                        this_termp.add_child(Tree::Leaf(tk_num));
+                        this_termp.add_child(termp);
+                        Some(Tree::Node(this_termp))
+                    }
+                    None => None,
+                },
+                None => None,
+            },
+            None => Some(Tree::Node(Node::new(NodeType::Expp))), // Epsilon
+        }
+    }
+
+    fn match_token(&mut self, ttype: TokenType) -> Option<&ExpToken> {
         let mut peek = self.source.peek();
-    
+
         while let Some(tok) = peek {
             if tok.ttype == TokenType::UNKNOWN || tok.ttype == TokenType::WS {
                 _ = self.source.next();
                 peek = self.source.peek();
-            }
-            else {
+            } else {
                 break;
             }
         }
 
-        self.source
-            .peek()
-            .filter(|next| next.ttype == ttype)
+        self.source.peek().filter(|next| next.ttype == ttype)
     }
 
-    fn consume(&mut self) -> Option<&ExpToken> { self.source.next() }
+    fn consume(&mut self) -> Option<ExpToken> {
+        self.source.next()
+    }
 
+    fn match_consume(&mut self, ttype: TokenType) -> Option<ExpToken> {
+        let tk = self.match_token(ttype);
+        if tk.is_some() {
+            self.consume()
+        } else {
+            None
+        }
+    }
 }
 
-/// Driver function that takes a string slice, tokenizes the slice using [super::lexer::ExpLexer], 
+/// Driver function that takes a string slice, tokenizes the slice using [super::lexer::ExpLexer],
 /// obtaining a [Vec] of [ExpToken], instantiating an [ExpParser] and performing the parse.
 pub fn parse(text: &str) -> bool {
     let tokens = tokenize(text);
-    let peeker = tokens.iter().peekable();
-    let mut parser = ExpParser :: new(peeker);
+    let it = tokens.into_iter().peekable();
+    let mut parser = ExpParser::new(it);
     parser.parse()
 }
 
@@ -167,5 +242,4 @@ mod tests {
         let result = parse(text);
         assert_eq!(result, true);
     }
-
 }
