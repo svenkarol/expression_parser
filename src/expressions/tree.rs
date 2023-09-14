8d3685604e00ee816982ae8b41a55131d4e9e881
Sven Karol <svenkarol+github@gmail.com>
2023-09-14 14:01:11 +0200

    Improve reference handling; support for parent nodes depending on context

--- src/expressions/tree.rs
+++ src/expressions/tree.rs
@@ -6,43 +6,95 @@ use std::rc::Rc;
 
 #[derive(Serialize)]
 pub struct Plus {
-    parent: Option<Box<BinExp>>,
+    #[serde(skip_serializing)]
+    parent: Option<PlusCtx>,
 }
 impl Plus {
-    pub fn new(parent: Option<Box<BinExp>>) -> Self {
+    pub fn new(parent: Option<PlusCtx>) -> Self {
         Plus { parent: parent }
     }
 }
 #[derive(Serialize)]
 pub struct Minus {
-    parent: Option<Box<BinExp>>,
+    #[serde(skip_serializing)]
+    parent: Option<MinusCtx>,
 }
 
 impl Minus {
-    pub fn new(parent: Option<Box<BinExp>>) -> Self {
+    pub fn new(parent: Option<MinusCtx>) -> Self {
         Minus { parent: parent }
     }
 }
 #[derive(Serialize)]
 pub struct Mult {
-    parent: Option<Box<BinExp>>,
+    #[serde(skip_serializing)]
+    parent: Option<MultCtx>,
 }
 
 impl Mult {
-    pub fn new(parent: Option<Box<BinExp>>) -> Self {
+    pub fn new(parent: Option<MultCtx>) -> Self {
         Mult { parent: parent }
     }
 }
 #[derive(Serialize)]
 pub struct Div {
-    parent: Option<Box<BinExp>>,
+    #[serde(skip_serializing)]
+    parent: Option<DivCtx>,
 }
 
 impl Div {
-    pub fn new(parent: Option<Box<BinExp>>) -> Self {
+    pub fn new(parent: Option<DivCtx>) -> Self {
         Div { parent: parent }
     }
 }
+
+#[derive(Serialize)]
+pub struct BinExp {
+    #[serde(skip_serializing)]
+    parent: Option<BinExpCtx>,
+    op: BinAOp,
+    left: AExp,
+    right: AExp,
+}
+
+impl BinExp {
+    pub fn new(op: BinAOp, left: AExp, right: AExp, parent: Option<BinExpCtx>) -> AstRef<Self> {
+        let exp = BinExp {
+            op: op,
+            parent: parent,
+            left: left,
+            right: right,
+        };
+        let exp_ref = AstRef::from_value(exp);
+        match &exp_ref.borrow_mut().left {
+            AExp::BinExp(lexp) => {
+                lexp.borrow_mut().parent = Some(BinExpCtx::BinExp(AstRef::from_ref(&exp_ref)))
+            }
+            AExp::Number(lnum) => {
+                lnum.borrow_mut().parent = Some(NumberCtx::BinExp(AstRef::from_ref(&exp_ref)))
+            }
+        };
+        return exp_ref;
+    }
+}
+
+#[derive(Serialize)]
+pub struct Number {
+    #[serde(skip_serializing)]
+    parent: Option<NumberCtx>,
+    value: i64,
+}
+
+impl Number {
+    pub fn new(value: i64, parent: Option<NumberCtx>) -> Self {
+        Number {
+            parent: parent,
+            value: value,
+        }
+    }
+}
+
+/// Operator choice
 #[derive(Serialize)]
 pub enum BinAOp {
     /// +
@@ -54,104 +106,71 @@ pub enum BinAOp {
     /// /
     Div(Div),
 }
+
+/// Operand choice
 #[derive(Serialize)]
-pub struct BinExp {
-    #[serde(skip_serializing)]
-    _parent: ParentRel<BinExp>,
-    op: BinAOp,
-    left: ChildRel<AExp>,
-    right: ChildRel<AExp>,
+pub enum AExp {
+    BinExp(AstRef<BinExp>),
+    Number(AstRef<Number>),
 }
 
-pub type ParentRel<T> = Option<RefCell<Rc<T>>>;
-pub type ChildRel<T> = RefCell<Rc<T>>;
+#[derive(Serialize)]
+pub enum PlusCtx {
+    BinExp(AstRef<BinExp>),
+}
+#[derive(Serialize)]
+pub enum MultCtx {
+    BinExp(AstRef<BinExp>),
+}
+#[derive(Serialize)]
+pub enum DivCtx {
+    BinExp(AstRef<BinExp>),
+}
+#[derive(Serialize)]
+pub enum MinusCtx {
+    BinExp(AstRef<BinExp>),
+}
+#[derive(Serialize)]
+pub enum NumberCtx {
+    BinExp(AstRef<BinExp>),
+}
+#[derive(Serialize)]
+pub enum BinExpCtx {
+    BinExp(AstRef<BinExp>),
+}
+
+pub type AstRef<T> = Rc<RefCell<T>>;
 
 pub trait RelHelper<T> {
     fn from_value(val: T) -> Self;
-    fn from_ref(rc: &Rc<T>) -> Self;
+    fn from_ref(rc: &Rc<RefCell<T>>) -> Self;
 }
 
-impl<T> RelHelper<T> for ChildRel<T> {
+impl<T> RelHelper<T> for AstRef<T> {
     fn from_value(val: T) -> Self {
-        RefCell::new(Rc::new(val))
+        Rc::new(RefCell::new(val))
     }
-    fn from_ref(rc: &Rc<T>) -> Self {
-        RefCell::new(rc.clone())
+    fn from_ref(rc: &Rc<RefCell<T>>) -> Self {
+        rc.clone()
     }
-}
-
-impl<T> RelHelper<T> for ParentRel<T> {
-    
-    fn from_value(val: T) -> Self {
-        Some(RefCell::new(Rc::new(val)))
-    }
-    fn from_ref(rc: &Rc<T>) -> Self {
-        Some(RefCell::new(rc.clone()))
-    }
-
-    /*fn new_from(val: &ChildRel<T>) -> Self {
-        let weak_ref = RefCell::new(Weak::new());
-        *weak_ref.borrow_mut() = Rc::downgrade(&*val.borrow_mut());
-        return Some(weak_ref);
-    }*/
-    // Side note: would be nice to use weak references when building relations to the parent
-    // However, we can only derive weak references when we have a strong one already. As we
-    // want the strong references to owned by a child's parent, this creates a chicken-egg-problem
-    // and basically urges one to construct the tree top-down with a definitive root. Another option
-    // would be to derive the parents after the tree was completed, which gives back freedom
-    // in tree construction but implies an extra pass over the tree.
-}
-
-impl BinExp {
-    pub fn new(op: BinAOp, left: AExp, right: AExp, _self_parent: Option<&Rc<BinExp>>) -> Self {
-        let exp = BinExp {
-            op: op,
-            _parent: None, // how to set this? See above and below ...
-            left: ChildRel::from_value(left),
-            right: ChildRel::from_value(right),
-        };
-        /*match *exp.left.borrow().as_ref() {
-            AExp::BinExp(lexp) => lexp.parent = ParentRel::from_ref(self_parent.)
-        }*/ 
-        // Problem: we cannot clone the parent-child-rc as these have different types ...
-        // and the parent may derive from different contexts
-        return exp;
-    }
-}
-
-#[derive(Serialize)]
-pub struct Number {
-    parent: Option<Box<BinExp>>,
-    value: i64,
-}
-
-impl Number {
-    pub fn new(value: i64, parent: Option<Box<BinExp>>) -> Self {
-        Number {
-            parent: parent,
-            value: value,
-        }
-    }
-}
-#[derive(Serialize)]
-pub enum AExp {
-    BinExp(BinExp),
-    Number(Number),
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::expressions::tree::RelHelper;
+
     use super::AExp;
     use super::BinExp;
     use super::Mult;
-    use super::Plus;
     use super::Number;
+    use super::Plus;
+    use super::AstRef;
 
     #[test]
     fn test_ast_creation() {
-        let num1 = AExp::Number(Number::new(0 as i64, None));
-        let num2 = AExp::Number(Number::new(8 as i64, None));
-        let num3 = AExp::Number(Number::new(15 as i64, None));
+        let num1 = AExp::Number(AstRef::from_value(Number::new(0 as i64, None)));
+        let num2 = AExp::Number(AstRef::from_value(Number::new(8 as i64, None)));
+        let num3 = AExp::Number(AstRef::from_value(Number::new(15 as i64, None)));
         let mul = AExp::BinExp(BinExp::new(
             super::BinAOp::Mult(Mult::new(None)),
             num2,
@@ -167,6 +186,6 @@ mod tests {
         let json = serde_json::to_string(&add_tree);
         assert!(json.is_ok());
         let json_txt = json.unwrap();
-        assert_eq!(("{\"op\":{\"Plus\":{\"parent\":null}},\"left\":{\"Number\":{\"parent\":null,\"value\":0}},\"right\":{\"BinExp\":{\"op\":{\"Mult\":{\"parent\":null}},\"left\":{\"Number\":{\"parent\":null,\"value\":8}},\"right\":{\"Number\":{\"parent\":null,\"value\":15}}}}}"), json_txt);
+        assert_eq!(("{\"op\":{\"Plus\":{}},\"left\":{\"Number\":{\"value\":0}},\"right\":{\"BinExp\":{\"op\":{\"Mult\":{}},\"left\":{\"Number\":{\"value\":8}},\"right\":{\"Number\":{\"value\":15}}}}}"), json_txt);
     }
 }
